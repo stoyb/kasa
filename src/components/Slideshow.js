d0eb46d1af15b7e519216c64e30e99b7bea7670c
stoyb <soniatoyb@yahoo.fr>
2023-10-25 16:23:03 +0200

    accomodation integration

---
+++ src/components/Slideshow.js
@@ -0,0 +1,31 @@
+import { React, useState } from 'react'
+import data from '../data.json'
+import { useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import styles from '../styles/Slideshow.module.css'
+
+const Slideshow = () => {
+  const { id } = useParams();
+  let item = data.find((item) => item.id === id)
+  let [currentIndex, setCurrentIndex] = useState(0);
+
+  function prevImage() {
+      setCurrentIndex(currentIndex === 0 ? item.pictures.length - 1 : currentIndex - 1)
+    } 
+  function nextImage() {
+    setCurrentIndex(currentIndex === item.pictures.length -1 ? 0 : currentIndex + 1)
+    } 
+    return (
+    <>
+        <div key={id} className={styles.container}>
+            <FontAwesomeIcon className={styles.iconPrev} icon={faChevronLeft} onClick={prevImage} />
+            <img className={styles.img} src={item.pictures[currentIndex]} alt="img"/>
+            <FontAwesomeIcon className={styles.iconNext} icon={faChevronRight} onClick={nextImage} />
+        </div>
+    </>
+  )
+}
+
+export default Slideshow
--- src/routes/accomodation.jsx
+++ src/routes/accomodation.jsx
@@ -1,28 +1,48 @@
 import React from 'react'
-//import styles from '../styles/accomodation.module.css'
+import styles from '../styles/accomodation.module.css'
 import Header from '../components/Header'
+import Slideshow from '../components/Slideshow'
 import { useParams } from 'react-router-dom'
 import data from '../data.json'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import Collapse from '../components/Collapse'
 
 const Accomodation = () => {
   const { id } = useParams();
+  let item = data.find((item) => item.id === id)
+  let rating = parseInt(item.rating)
     return (
     <>
     <Header/>
-     <div>
-         {data
-         .filter((item) => item.id === id)
-         .map((item) => (
-             <div key={id}>
-                 <div>
-                     {item.pictures.map((picture)=> (
-                         <img src={picture}/>
-                     ))}
-                </div>
-                 <p>{item.title}</p>
-             </div>
-         ))
-         }
+    <Slideshow/>
+     <div className={styles.container}>
+       <div>
+          <h1 className={styles.title}>{item.title}</h1>
+          <p className={styles.location}>{item.location}</p>
+          <div className={styles.blocTags}>{item.tags.map((tag,index) => 
+            <p className={styles.tag} key={index}>{tag}</p>
+            )}
+          </div>
+       </div>
+       <div>
+          <div> 
+            { [1, 2, 3, 4, 5].map((star, index) =>  
+              <span key={index} className={rating >= star ? styles.starPink : styles.starGrey}><FontAwesomeIcon icon={faStar} /></span>
+            )}
+          </div>
+           <div>
+            <p>{item.host.name}</p>
+            <img src={item.host.picture} alt="ImgHost"/>
+          </div> 
+       </div>
+       <div>
+         <Collapse title="Description" text={item.description} />
+         <Collapse title="Équipements" text={item.equipments.map((equi) => <li>{equi}</li>)} />
+       </div>
+       
+
+       
      </div>
     </>
   )
