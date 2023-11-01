9993423ccff7f9ddc744cde4349cf200e9ff2208
stoyb <soniatoyb@yahoo.fr>
2023-11-01 18:18:28 +0100

    new deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import logo from '../assets/logoHeader.png'
 import styles from '../styles/Header.module.css'
 
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <>
     <header className={styles.header}>
@@ -12,8 +14,8 @@ const Header = () => {
             <img src={logo} className={styles.logo} alt="Logo"/>
         </Link>
         <nav className={styles.nav}>
-            <NavLink to="/" className={styles.navHome} >Accueil</NavLink>
-            <NavLink to="/about" className={styles.navAbout}  >A propos</NavLink>
+            <NavLink to="/" className={location.pathname === '/' ? styles.navHomeActive : styles.navHome}>Accueil</NavLink>
+            <NavLink to="/about" className={location.pathname === '/about' ? styles.navAboutActive : styles.navAbout}>A propos</NavLink>
         </nav>
     </header>
     </>
